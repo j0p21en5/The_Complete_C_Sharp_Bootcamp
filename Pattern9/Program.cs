63c3eb2850656fbd4e50adc6980a72d61a0d0c25
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-07 20:06:50 +0530

    update

---
+++ Pattern9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    Console.Write("-");
+                }
+                Console.Write("*");
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("-");
+                }
+                Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
