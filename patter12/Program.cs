da7ced6c3a947a382d109a5fa99f1820a4e28c9a
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-07 23:47:23 +0530

    update

---
+++ patter12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patter12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = i; j < 4; j++)
+                {
+                    Console.Write(" ");
+                }
+                
+                Console.Write("*");
+
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                Console.WriteLine();
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("*");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
