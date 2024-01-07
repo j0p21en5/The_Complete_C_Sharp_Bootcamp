b7ac8b9551b27d323f3b376b40427487ab881dca
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-07 18:36:56 +0530

    update

--- Pattern4/Program.cs
+++ Pattern4/Program.cs
@@ -12,21 +12,43 @@ namespace Pattern4
 
         static void Main(string[] args)
         {
-            string spc = " ";
-            int n = 5;
-            for (int i = 1; i <= n; i++)
+            int num ;
+            Console.WriteLine("Enter the number");
+            num = int.Parse(Console.ReadLine());
+            int n = num-1;
+            
+            for (int  i = 0; i < num; i++)
             {
+
+                
+                for (int j = i; j < n; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("*");
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("*");
+                }
                 
                 for (int j = 1; j <= i; j++)
                 {
-                    
-
                     Console.Write("*");
-                     
-
                 }
+                
                 Console.WriteLine();
+
             }
+            
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
             Console.ReadLine();
         }
     }
