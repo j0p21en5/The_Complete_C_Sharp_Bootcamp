a143b41ef9a656b51986a83401989acd844a35b1
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-07 23:32:22 +0530

    update

---
+++ Pattern11_sqr/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern11_sqr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the number : ");
+            int n =int.Parse(Console.ReadLine());
+
+            int s = n - 1;
+            int t = n - 2;
+            for (int i = 0; i <= n; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            for (int i = 0;i < t; i++)
+            {
+                
+                Console.Write("*");
+                for (int j = 0; j < s; j++)
+                {
+                    Console.Write(" ");
+
+
+                }
+                Console.Write("*");
+                Console.WriteLine();
+
+
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                Console.Write("*");
+            }
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    Console.Write("*");
+
+            //}
+
+
+            Console.ReadLine();
+        }
+    }
+}
