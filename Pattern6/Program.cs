b044503f809bf897431e8e4ff88dd4f559cac8d4
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-07 19:17:05 +0530

    update

---
+++ Pattern6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number : ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            { 
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                
+                Console.WriteLine();
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
