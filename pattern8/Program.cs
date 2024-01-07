cb1c6bee9b9523a65406444da971ff2f4d57ac4e
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-07 19:51:16 +0530

    update

---
+++ pattern8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int n = int.Parse(Console.ReadLine());
+            int s = n - 1;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= s; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 2; j <= i; ++j)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = 2; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = i; k <= n; k++)
+                {
+                    Console.Write("*");
+                }
+                for (int k = i; k <= s; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
