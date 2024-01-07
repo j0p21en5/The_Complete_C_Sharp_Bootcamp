a4b2bff80cd5fbb56248de214118d84fe68a3e08
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-07 18:14:20 +0530

    update

---
+++ Pattern4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string spc = " ";
+            int n = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                
+                for (int j = 1; j <= i; j++)
+                {
+                    
+
+                    Console.Write("*");
+                     
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
