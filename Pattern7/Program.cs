bcbe6f87c4a0bfa1b89067b68ae0774e6ae7bd1a
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-07 19:33:47 +0530

    update

--- Pattern7/Program.cs
+++ Pattern7/Program.cs
@@ -10,6 +10,35 @@ namespace Pattern7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number");
+            int n = int.Parse(Console.ReadLine());
+            int s = n - 1;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= s; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = 2; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = i; k <= n; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
