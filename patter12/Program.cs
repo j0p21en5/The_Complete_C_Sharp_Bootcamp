a6807a6bb4c9cbfb9e33a299718ac7bf1e65da2e
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 09:45:12 +0530

    update

--- patter12/Program.cs
+++ patter12/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace patter12
@@ -10,41 +11,62 @@ namespace patter12
     {
         static void Main(string[] args)
         {
-           
-            Console.WriteLine("Enter the number : ");
-            int n = int.Parse(Console.ReadLine());
-            int s = n - 1;
-            int t = n - 2;
-            int q = n + 1;
 
-            for (int i = 0; i < s; i++)
+            //Console.WriteLine("Enter the number : ");
+            //int n = int.Parse(Console.ReadLine());
+            //int s = n - 1;
+            //int t = n - 2;
+            //int q = n + 1;
+
+            //for (int i = 0; i < s; i++)
+            //{
+            //    for (int j = i; j < s; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    Console.Write("*");
+
+            //    for (int k = 0; k < i; k++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 0; k < i; k++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.Write("*");
+            //    Console.WriteLine();
+
+            //}
+            //for (int i = 0; i < q; i++)
+            //{
+            //    Console.Write("*");
+            //    Console.Write(" ");
+
+            //}
+
+            int n = 12;
+            int count = 0;
+            for (int i = 2; i < n; i++)
             {
-                for (int j = i; j < s; j++)
-                {
-                    Console.Write(" ");
-                }
-                
-                Console.Write("*");
+                Console.WriteLine(i);
 
-                for (int k = 0; k < i; k++)
+                if(n%i == 0)
                 {
-                    Console.Write(" ");
+                    count++;
+
                 }
-                for (int k = 0; k < i; k++)
-                {
-                    Console.Write(" ");
-                }
-                Console.Write("*");
-                Console.WriteLine();
 
             }
-            for (int i = 0; i < q; i++)
+            if(count == 1)
             {
-                Console.Write("*");
-                Console.Write(" ");
-
+                Console.WriteLine("it is not prime");
             }
-
+            else
+            {
+                Console.WriteLine("it is prime");
+            }
             Console.ReadLine();
         }
     }
