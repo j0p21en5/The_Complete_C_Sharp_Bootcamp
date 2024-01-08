fdf3bae178718551c64e974b4d73e39896aea342
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 21:03:24 +0530

    update

--- Patterns/ARRAY/Sum Of N Numbers/Program.cs
+++ Patterns/ARRAY/Sum Of N Numbers/Program.cs
@@ -12,7 +12,7 @@ namespace Sum_Of_N_Numbers
         {
             Console.WriteLine("Enter the number you want to store in Array : ");
             int n = int.Parse(Console.ReadLine());
-
+            int sum = 0;
             int[] arr = new int[n]; 
 
             Console.WriteLine("Enter the Array elements ; ");
@@ -24,6 +24,13 @@ namespace Sum_Of_N_Numbers
 
             }
             Console.WriteLine("Array elements Are : ");
+            for(int i = 0;i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+                sum = sum + arr[i];
+            }
+            Console.WriteLine("The sum of N number is : "+sum);
+            Console.ReadLine();
         }
     }
 }
