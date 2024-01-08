1c711fdbe35425588d994d6773dbec4a1be5fe89
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 10:56:25 +0530

    update

--- Patterns/ARRAY/ConsoleApp1/Program.cs
+++ Patterns/ARRAY/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[5];
+            int sum = 0;
+            Console.WriteLine("Enter Five Students Marks : ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                arr[i] = b;
+            }
+            Console.WriteLine("Five Students Marks Is : ");
+            for(int i = 0;i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+                sum = sum + arr[i];
+            }
+
+            double avg = sum / 5;
+            Console.WriteLine("Average Marks Is : "+avg);
+            Console.ReadLine();
         }
     }
 }
