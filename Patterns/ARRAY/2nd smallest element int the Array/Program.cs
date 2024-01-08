d0bafd662d466a3315fe842cdd46dca61d6c01c2
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 21:51:51 +0530

    update

---
+++ Patterns/ARRAY/2nd smallest element int the Array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_smallest_element_int_the_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
