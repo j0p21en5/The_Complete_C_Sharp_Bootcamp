4b8e081e169e245d94631009d4c36e9a6071781d
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 10:02:41 +0530

    Add count number

---
+++ Count_Number_Of_Digits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Number_Of_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 5, 2, 3,3,3 };
+            
+            Console.WriteLine("Enter A to find Count : ");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == n)
+                {
+                    count++;
+                }
+
+            }
+            Console.WriteLine("The Number is Count : "+count);
+            Console.ReadLine();
+
+        }
+    }
+}
