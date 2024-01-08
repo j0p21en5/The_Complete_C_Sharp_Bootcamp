a58985ab91da54bdd3c9dadbbaa593969755337f
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 09:49:07 +0530

    update

---
+++ Prime_Or_Not/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Or_Not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check : ");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 2; i < n; i++)
+            {
+                Console.WriteLine(i);
+
+                if (n % i == 0)
+                {
+                    count++;
+
+                }
+
+            }
+            if (count == 1)
+            {
+                Console.WriteLine("it is not prime");
+            }
+            else
+            {
+                Console.WriteLine("it is prime");
+            }
+            Console.ReadLine();
+        }
+    }
+}
