c07b2d2fab2babb987fbd7db19dd28f70943527f
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 11:04:44 +0530

    ADD Print A String

---
+++ Patterns/ARRAY/Print A String Using Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_A_String_Using_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "Jaykumar" };
+            Console.WriteLine(arr.Length); 
+            
+            foreach (string i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
