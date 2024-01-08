95728a075a9809596f424da1e31167e346abe132
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 11:11:15 +0530

    update

---
+++ Patterns/ARRAY/Input sting and print in Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Input_sting_and_print_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number you want to store in array string : ");
+            int n  = int.Parse(Console.ReadLine());
+
+            string[] arr = new string[n];
+            Console.WriteLine("Input the String : ");
+            for (int i = 0;i<arr.Length;i++)
+            {
+                string b =(Console.ReadLine());
+                arr[i] = b;
+            }
+            Console.WriteLine("Strings Are : ");
+            foreach(string i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
