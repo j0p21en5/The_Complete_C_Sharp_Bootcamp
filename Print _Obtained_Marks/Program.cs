e6a1d81cc5629e0184eb99097b86c409a47645ec
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-08 10:25:36 +0530

    update

---
+++ Print _Obtained_Marks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print__Obtained_Marks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("Enter 5 Students Marks : ");
+            for (int i = 0; i < 5; i++)
+            {
+                
+                Console.Write("Enter {0} Index Student Mark : ",i);
+                int b = int.Parse(Console.ReadLine());
+                arr[i] = b;
+            }
+            Console.WriteLine("The Marks Obtained By Student Are : ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("The Marks Obtained By Student is :  "+arr[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
