00ad19a99cedc81364246ebdb3ac83b4c845689d
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-10 00:24:33 +0530

    update

---
+++ Patterns/ARRAY/Array element in reverse order/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_element_in_reverse_order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the element number you want to store in Array : ");
+            int n = int.Parse(Console.ReadLine()); 
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the Array elements : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                arr[i] = b;
+            }
+
+            Console.WriteLine("The Reverse elements Are : ");
+
+            for (int i = arr.Length-1; i >=0; i++)
+            {
+                Console.WriteLine(arr[i]);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
