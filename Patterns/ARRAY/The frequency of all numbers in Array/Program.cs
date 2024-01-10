8a0ffcab13a956fd1a0ce8266c4c00106296f0f5
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-10 18:39:39 +0530

    update

--- Patterns/ARRAY/The frequency of all numbers in Array/Program.cs
+++ Patterns/ARRAY/The frequency of all numbers in Array/Program.cs
@@ -10,6 +10,36 @@ namespace The_frequency_of_all_numbers_in_Array
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number of elements you want to store in array : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the Array elements : ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int inp = int.Parse(Console.ReadLine());
+                arr[i] = inp;
+            }
+
+            for (int i = 0;i < arr.Length; i++)
+            {
+                int count = 1;
+
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+
+
+                }
+                Console.WriteLine(arr[i]+" Count in"+count +" Times");
+
+            }
+            Console.ReadLine();
         }
     }
 }
