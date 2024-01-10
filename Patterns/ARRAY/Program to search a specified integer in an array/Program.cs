7a38de592c766588e2dbee0634559e6f9717f473
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-10 22:18:48 +0530

    update

---
+++ Patterns/ARRAY/Program to search a specified integer in an array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Program_to_search_a_specified_integer_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements you want to store in Array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the arry elements : ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int inp = int.Parse(Console.ReadLine());
+                arr[i] = inp;
+            }
+            Console.WriteLine("Enter the element to found : ");
+            int fon = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0;i < arr.Length; i++)
+            {
+                if (arr[i] == fon)
+                {
+                    Console.WriteLine("Element found at index : "+i);
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            if(count>0)
+            {
+                Console.WriteLine("Element not found...");
+            }
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
