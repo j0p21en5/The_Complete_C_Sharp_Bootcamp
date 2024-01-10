bf8b5f95855ad0264ae5bd4263d1deb1e11a2a4a
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-11 00:22:05 +0530

    add new

---
+++ Patterns/ARRAY/Remove duplicate elements in an array/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_duplicate_elements_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eneter the you want to store in array ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter Array elements : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int b = int.Parse(Console.ReadLine());
+                arr[i] = b;
+            }
+            Console.WriteLine("After......");
+            for (int i = 0;i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] != arr[j])
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                } 
+
+                    
+                        
+                
+            }
+            //Console.WriteLine("After......");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+
+            //}
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
