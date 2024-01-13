3e3a1088a9a22ee24896418e8b4e3a9b0eb101a6
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-13 11:48:02 +0530

    update

---
+++ Patterns/ARRAY/Reversing Array using Swapping Method/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversing_Array_using_Swapping_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            int temp= arr.Length;
+            int temp2;
+
+
+            while (temp >= 0)
+            {
+                for (int j = 0; j < temp - 1; j++)
+                {
+                    temp2 = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp2;
+                }
+                temp--;
+            }
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
