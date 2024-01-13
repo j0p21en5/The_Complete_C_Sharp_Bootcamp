09e513f0d932dc7f5342628d99a5198d8c776b00
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-13 18:27:52 +0530

    update

---
+++ Patterns/ARRAY/Find the index of an element in an array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_index_of_an_element_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 ,5,6,8};
+
+            int n = 2;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == n)
+                {
+                    Console.WriteLine("Search element "+n+" Index is "+i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
