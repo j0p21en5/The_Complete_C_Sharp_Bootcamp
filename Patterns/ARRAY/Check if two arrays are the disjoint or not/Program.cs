c96010c09e1562510dece3d635e48a6261dbe588
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-13 17:54:10 +0530

    update

---
+++ Patterns/ARRAY/Check if two arrays are the disjoint or not/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_if_two_arrays_are_the_disjoint_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 5, 25 };
+            int[] arr2 = { 4,65,55,45,1 };
+
+            int count = 0; 
+             
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        count++; 
+                    }
+                }
+            }
+
+            if(count== 0)
+            {
+                Console.WriteLine("This is disjoint...");
+            }
+            else
+            {
+                Console.WriteLine("This is not disjoint");
+            }
+            Console.ReadLine();
+        }
+    }
+}
