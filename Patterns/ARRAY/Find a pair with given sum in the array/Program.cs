5fa0f2923d737ad0d5466e361c25466fb3c11fa7
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-13 22:06:14 +0530

    update

---
+++ Patterns/ARRAY/Find a pair with given sum in the array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_a_pair_with_given_sum_in_the_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 5, 7, 85, 64, 12, 6};
+            int sum = 18;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1;j<arr.Length; j++)
+                {
+                    if (arr[i] + arr[j] == sum)
+                    {
+                        Console.WriteLine(arr[i]+" , " + arr[j]);
+                        
+                    }
+                    
+                    
+                }
+                
+                
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
