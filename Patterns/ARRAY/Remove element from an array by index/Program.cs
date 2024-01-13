7039a5f233e6618bb68b09ad2d0e2ab5a5868630
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-13 20:08:33 +0530

    update

---
+++ Patterns/ARRAY/Remove element from an array by index/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_element_from_an_array_by_index
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr= { 1, 2, 3 ,4,5 };
+            int n = 2;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(i == n)
+                {
+                    continue;
+                }
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
