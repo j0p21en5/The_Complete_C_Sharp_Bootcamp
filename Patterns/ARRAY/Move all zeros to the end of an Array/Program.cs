dddb9e85104162115b0d80d2547c1fb61fcc5a76
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-13 23:49:43 +0530

    update

---
+++ Patterns/ARRAY/Move all zeros to the end of an Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move_all_zeros_to_the_end_of_an_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 1, 0, 6, 0, 4, 0 };
+            int n = arr.Length-1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    arr[arr.Length-n] = arr[i];
+                    Console.WriteLine(arr[i]);
+                    n--;
+                }
+
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
