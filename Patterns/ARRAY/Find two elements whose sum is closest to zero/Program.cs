ef4677bc52302c2492ac3c6b59a641f08785a757
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-13 22:30:11 +0530

    update

---
+++ Patterns/ARRAY/Find two elements whose sum is closest to zero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_two_elements_whose_sum_is_closest_to_zero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 5, 65, 4, 2, 41, 21, 1 };
+
+            int sum;
+
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    sum  = arr[i] + arr[j];
+                }
+            }
+        }
+    }
+}
