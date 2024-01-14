415ae0578c5e544ef370a5c6e2ff2d7ebc90c540
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-14 12:57:43 +0530

    update

---
+++ Patterns/ARRAY/FInd the maximum product of three number in array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FInd_the_maximum_product_of_three_number_in_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 5, 6, 41, 2, 5, 1, 2, };
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] >= arr[j + 1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            //foreach (int i in arr)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int product = arr[arr.Length-3] *arr[arr.Length - 2] * arr[arr.Length - 1];
+            Console.WriteLine("Pair with maximum product in array is : "+ arr[arr.Length - 3] + " , " + arr[arr.Length - 2] + " , " + arr[arr.Length - 1] + " = " + product);
+            Console.ReadLine();
+        }
+    }
+}
