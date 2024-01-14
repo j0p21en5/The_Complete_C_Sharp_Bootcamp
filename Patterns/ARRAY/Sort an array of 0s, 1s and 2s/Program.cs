336ed57cff57d51a3eed15d6382ce47601e2e9bd
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-14 19:46:31 +0530

    update

---
+++ Patterns/ARRAY/Sort an array of 0s, 1s and 2s/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_an_array_of_0s__1s_and_2s
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 0, 1, 0, 2, 0, 0, 2, 1 };
+
+            int count1 = 0, count2 = 0; 
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                continue;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 1)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                continue;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 2)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                continue;
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ Patterns/ARRAY/finding the length of an array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finding_the_length_of_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            int len =1;
+
+            for (int i = 0; i < len+1; i++)
+            {
+                //if (arr[i] == null)
+                //{
+                //    break;
+                //}
+                len++;
+            }
+            //foreach (int i in arr)
+            //{
+            //    len++;
+            //}
+            Console.WriteLine("Length of Array is ; "+len);
+            Console.ReadLine();
+        }
+    }
+}
