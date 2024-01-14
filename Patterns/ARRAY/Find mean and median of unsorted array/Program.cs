42150a6e60f6510e5de9a166490752718cc1d1f6
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-14 12:14:23 +0530

    update

--- Patterns/ARRAY/Find mean and median of unsorted array/Program.cs
+++ Patterns/ARRAY/Find mean and median of unsorted array/Program.cs
@@ -10,14 +10,17 @@ namespace Find_mean_and_median_of_unsorted_array
     {
         static void Main(string[] args)
         {
-            int[] arr = { 4, 3, 7, 5, 6, 2, };
+            int[] arr = { 4, 3, 7, 5, 6, 2 };
             double sum = 0,mean;
             for (int i = 0; i < arr.Length; i++)
             {
                 sum = sum + arr[i];
 
             }
+
             mean= sum / arr.Length;
+
+
             Console.WriteLine("Mean Of The Array is : "+mean);
             Console.ReadLine();
         }
