f01a8d3a2894b00204790b9ecb5973737e1be89d
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-14 12:21:46 +0530

    update

---
+++ Patterns/ARRAY/Segregate 0s and 1s in an array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Segregate_0s_and_1s_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 0, 0, 1, 1, 1, 0, 0 };
+
+            int count = 0;
+
+            Console.WriteLine("After segregate 0s and 1s in an Array, Array is : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 1)
+                {
+                    count++;
+                    continue;
+                }
+                Console.WriteLine(arr[i]);
+            }
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(1);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
