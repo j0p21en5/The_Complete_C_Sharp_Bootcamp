086893037fe9af65b14369514390effa955598fc
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-14 11:17:36 +0530

    update

---
+++ Patterns/ARRAY/Move all zeros to the Start of an Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move_all_zeros_to_the_Start_of_an_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 5, 0, 8, 0, 3, 0, 4, 0 };
+            int count = 0;
+           
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    count++;
+                    continue;
+                }
+                
+            }
+            Console.WriteLine("After moving all zero to the start");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(0);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    
+                    continue;
+                }
+                Console.WriteLine(arr[i]);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
