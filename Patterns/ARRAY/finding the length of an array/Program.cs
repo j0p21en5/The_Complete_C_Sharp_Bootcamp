3334e6be5a26e2f71d3232682a268a25af25cc99
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-14 19:47:40 +0530

    update
    add code for finding the length of Array

--- Patterns/ARRAY/finding the length of an array/Program.cs
+++ Patterns/ARRAY/finding the length of an array/Program.cs
@@ -12,20 +12,20 @@ namespace finding_the_length_of_an_array
         {
             int[] arr = { 1, 2, 3, 4, 5, 6 };
 
-            int len =1;
+            int len =0 ;
 
-            for (int i = 0; i < len+1; i++)
-            {
-                //if (arr[i] == null)
-                //{
-                //    break;
-                //}
-                len++;
-            }
-            //foreach (int i in arr)
+            //for (int i = 0; i < len+1; i++)
             //{
+            //    //if (arr[i] == null)
+            //    //{
+            //    //    break;
+            //    //}
             //    len++;
             //}
+            foreach (int i in arr)
+            {
+                len++;
+            }
             Console.WriteLine("Length of Array is ; "+len);
             Console.ReadLine();
         }
