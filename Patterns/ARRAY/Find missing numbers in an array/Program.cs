5069df978f5752daa26dd141a529f86f4c8297df
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-15 11:46:50 +0530

    update

---
+++ Patterns/ARRAY/Find missing numbers in an array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_missing_numbers_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 5, 4, 1, 7 };
+
+            int temp = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0;j<arr.Length-1; j++)
+                {
+                    if (arr[j] >= arr[j+1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j+1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+
+              
+            }
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Patterns/ARRAY/Minimum difference between two elements in an array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_difference_between_two_elements_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 54, 24, 3, 42, 5, 6, 21, 2, 54, };
+
+            int temp = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0;j<arr.Length-1; j++)
+                {
+                    if (arr[j] - arr[j+1] >= temp ) 
+                    {
+                        Console.WriteLine(arr[j] +" " + arr[j+1]);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
