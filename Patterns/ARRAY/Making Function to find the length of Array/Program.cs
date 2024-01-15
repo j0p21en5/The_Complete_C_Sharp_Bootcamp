8b88001a539ba196a4c706d8ae39c67f62a79c92
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-15 13:09:43 +0530

    update

---
+++ Patterns/ARRAY/Making Function to find the length of Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Making_Function_to_find_the_length_of_Array
+{
+    class length
+    {
+        public int[] arr = {2,5,6,78,2,15,5};
+
+
+        int count = 0;
+        
+        public void arrLength()
+        {
+            foreach (int i in arr)
+            {
+                count++;
+            }
+            Console.WriteLine("Length of Array is : "+count);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            length obj = new length();
+            
+            obj.arrLength();
+
+            Console.ReadLine();
+        }
+    }
+}
