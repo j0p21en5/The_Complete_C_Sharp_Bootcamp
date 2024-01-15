d6e5c03efde4b9f688d12f5b738f26b2dff831bd
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-01-15 12:28:33 +0530

    update

---
+++ Patterns/ARRAY/Function of sum of all array element/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_of_sum_of_all_array_element
+{
+    //class arrsum
+    //{
+        
+
+        
+    //}
+    internal class Program
+    {
+        
+        
+        public int[] arr = new int[5];
+        public void inpfun()
+        {
+            Console.WriteLine("Enter Aray element : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                int inp = int.Parse(Console.ReadLine());
+                arr[i] = inp;
+            }
+
+        }
+
+        public int sum = 0;
+
+        public void sumfun()
+        {
+            foreach (int item in arr)
+            {
+                sum = sum + item;
+            }
+            Console.WriteLine("sum is : " + sum);
+        }
+        static void Main(string[] args)
+        {
+
+            Program obj = new Program();
+            obj.inpfun();
+            obj.sumfun();
+
+            Console.ReadLine();
+        }
+    }
+}
