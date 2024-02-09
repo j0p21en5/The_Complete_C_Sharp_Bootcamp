49d08cd7b71c0605bf07daf3c084604fbaaa1fcc
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-02-09 11:00:41 +0530

    update

---
+++ Patterns/ARRAY/2D Array strt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Array_strt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3]
+            {
+                {0 ,1,3},
+                {4,5,2 } ,
+                {6,7,8 } ,
+            };
+
+            Console.WriteLine(arr[1,2]);
+            Console.WriteLine(arr.Rank);
+
+            Console.WriteLine(arr.GetLength(1));
+
+            Console.WriteLine("Accesing All the array elements using for loop...");
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(" "+arr[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
