0a2d27d257f8dab3c1376cb998c7d676783b5298
jay patel <132779573+j0p21en5@users.noreply.github.com>
2024-02-09 11:43:12 +0530

    update

--- Patterns/ARRAY/2D Array strt/Program.cs
+++ Patterns/ARRAY/2D Array strt/Program.cs
@@ -10,17 +10,28 @@ namespace _2D_Array_strt
     {
         static void Main(string[] args)
         {
-            int[,] arr = new int[3, 3]
+            //Rectangular Array ...
+            int[,] arr = new int[3, 3];
+            //{
+            //    {0 ,1,3},
+            //    {4,5,2 } ,
+            //    {6,7,8 } ,
+            //};
+            Console.WriteLine("Enter Array elements  : ");
+            for (int i = 0; i < arr.GetLength(0); i++)
             {
-                {0 ,1,3},
-                {4,5,2 } ,
-                {6,7,8 } ,
-            };
+                Console.WriteLine("Enter element of row : "+i);
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    int b = int.Parse(Console.ReadLine());
+                    arr[i, j] = b;
+                }
+            }
 
-            Console.WriteLine(arr[1,2]);
-            Console.WriteLine(arr.Rank);
+            //Console.WriteLine(arr[1,2]);
+            //Console.WriteLine(arr.Rank);
 
-            Console.WriteLine(arr.GetLength(1));
+            //Console.WriteLine(arr.GetLength(1));
 
             Console.WriteLine("Accesing All the array elements using for loop...");
 
@@ -28,10 +39,40 @@ namespace _2D_Array_strt
             {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                    Console.Write(" "+arr[i,j]);
+                    Console.Write(" " + arr[i, j]);
                 }
                 Console.WriteLine();
             }
+
+
+            //foreach(int i in arr)
+            //{
+            //    Console.WriteLine(i);
+
+            //
+
+
+            //JAGGED ARRAY
+
+            //int[][] arr = new int[3][];
+
+            //arr[0] = new[] { 1, 2, 3 };
+            //arr[1] = new[] { 5, 6, 7, 4, 6 };
+            //arr[2] = new[] { 6, 8, 6, 4, 7, 8 };
+
+            //Console.WriteLine(arr[0][2]);
+
+            //Console.WriteLine("accesing jaggd array using for loop.......");
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr[i].Length; j++)
+            //    {
+            //        Console.Write(" "+arr[i][j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
             Console.ReadLine();
 
         }
